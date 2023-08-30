91d511da83eaedeb40db57eeefdd948933c9638e
Carl595 <105862361+Carl595@users.noreply.github.com>
2023-08-30 14:33:48 +0200

    made img test

--- test.py
+++ test.py
@@ -78,6 +78,21 @@ class TestHemsida(TestCase):
         self.browser.find_element(By.ID, "X").click()
         self.browser.find_element(By.ID, "Facebook").click()
 
+
+    def testImageLoading(self):
+        self.browser.get(path.join((getcwd()), 'index.html'))
+        image_elements = self.browser.find_elements(By.TAG_NAME, "img")
+
+        for image_element in image_elements:
+            is_loaded = self.browser.execute_script("return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0;", image_element)
+
+            if is_loaded:
+                print(f"Image '{image_element.get_attribute('src')}' is loaded.")
+            else:
+                self.fail(f"Image '{image_element.get_attribute('src')}' is not loaded.")
+
+
+
     
 # denna bit finns här så att testerna körs om filen körs som vanligt python-program
 if __name__ == '__main__':
