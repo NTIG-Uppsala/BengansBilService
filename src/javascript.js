731f46d7aadaf7b0ac73bb9c5719d530f566aadd
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-08-31 13:45:20 +0200

    color öppetider

--- src/javascript.js
+++ src/javascript.js
@@ -6,29 +6,36 @@ const day = date.getDay()
 if (day < 6){
   if (hour >= 10 && hour < 16){
     document.addEventListener("DOMContentLoaded", function() {
-      document.getElementsByClassName("storeState")[0].innerHTML = "Just nu har vi Öppet";
+      document.getElementsByClassName("storeState")[0].innerHTML = "Öppet";
+      document.getElementsByClassName("storeState")[0].style.color = "green"
     });
   }
   else{
     document.addEventListener("DOMContentLoaded", function() {
-      document.getElementsByClassName("storeState")[0].innerHTML = "Just nu har vi Stängt";
+      document.getElementsByClassName("storeState")[0].innerHTML = "Stängt";
+      document.getElementsByClassName("storeState")[0].style.color = "red"
     });
   }
 }
 else if (day == 6){
   if (hour >= 10 && hour < 15){
     document.addEventListener("DOMContentLoaded", function() {
-      document.getElementsByClassName("storeState")[0].innerHTML = "Just nu har vi Öppet";
+      document.getElementsByClassName("storeState")[0].innerHTML = "Öppet";
+      document.getElementsByClassName("storeState")[0].style.color = "green"
+
     });
   }
   else{
     document.addEventListener("DOMContentLoaded", function() {
-      document.getElementsByClassName("storeState")[0].innerHTML = "Just nu har vi Stängt";
+      document.getElementsByClassName("storeState")[0].innerHTML = "Stängt";
+      document.getElementsByClassName("storeState")[0].style.color = "red"
+
     });
   }
 }
 else if (day == 7){
   document.addEventListener("DOMContentLoaded", function() {
-    document.getElementsByClassName("storeState")[0].innerHTML = "Just nu har vi Stängt";
+    document.getElementsByClassName("storeState")[0].innerHTML = "Stängt";
+    document.getElementsByClassName("storeState")[0].style.color = "red"
   });
 }
