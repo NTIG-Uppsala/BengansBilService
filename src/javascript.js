e15ec96e3962f4946f72eebeb026d3909ebde43e
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-08-31 14:19:48 +0200

    icon

--- src/javascript.js
+++ src/javascript.js
@@ -2,7 +2,7 @@ const date = new Date();
 const hour = date.getHours()
 const day = date.getDay()
 
-
+//If weekday
 if (day < 6){
   if (hour >= 10 && hour < 16){
     document.addEventListener("DOMContentLoaded", function() {
@@ -16,7 +16,7 @@ if (day < 6){
       document.getElementsByClassName("storeState")[0].style.color = "red"
     });
   }
-}
+} //Saturday
 else if (day == 6){
   if (hour >= 10 && hour < 15){
     document.addEventListener("DOMContentLoaded", function() {
@@ -32,7 +32,7 @@ else if (day == 6){
 
     });
   }
-}
+}//Sunday
 else if (day == 7){
   document.addEventListener("DOMContentLoaded", function() {
     document.getElementsByClassName("storeState")[0].innerHTML = "Stängt";
